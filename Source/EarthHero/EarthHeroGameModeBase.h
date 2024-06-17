491131d1c402cedf9a9ebe8d70086f1e23c949f1
Plagem <dasankim2000@gmail.com>
2024-06-18 04:10:09 +0900

    Initial commit

---
+++ Source/EarthHero/EarthHeroGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EarthHeroGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EARTHHERO_API AEarthHeroGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
