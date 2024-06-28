dbdc80457f71c23bfcd5e47bf7339e9cf5ffac7c
이승언 <71816852+grukall@users.noreply.github.com>
2024-06-29 01:24:08 +0900

    stat 수정

--- Source/EarthHero/Stat/StatStructure.h
+++ Source/EarthHero/Stat/StatStructure.h
@@ -12,9 +12,6 @@ struct FStatStructure : public FTableRowBase
 	GENERATED_BODY()
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
-	USkeletalMesh *WeaponMesh = nullptr;	//무기 스켈레탈 매쉬
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
 	float MaxHealth = 100.f;				//최대 체력
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
@@ -24,35 +21,30 @@ struct FStatStructure : public FTableRowBase
 	float HealthRegeneration = 0.f;			//체력 재생
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
-	float AttackPower = 0.f;				//공격력
-
+	float MovementSpeed = 0.f;				//이동속도
+	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
-	float Damage = 0.f;						//데미지
+	float NormalDamage = 0.f;				//일반 공격 데미지
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
 	float AttackSpeed = 0.f;				//공격 속도
-	
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
-	float MovementSpeed = 0.f;				//이동 속도
+	float SkillDamage = 0.f;				//스킬 데미지
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
 	float DefensePower = 0.f;				//방어력
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
-	float MaxEXP = 0.f;						//최대 경험치
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
-	float SkillRange = 0.f;					//스킬 범위
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
-	float ReduceCoolTime = 0.f;				//쿨타임 감소
+	float MaxExp = 0.f;						//최대 경험치
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
-	bool HasInvincibilityAbility = 0.f;		//무적 능력
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
-	float Barrier = 0.f;					//베리어
+	float Exp = 0.f;
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
-	int32 Summon = 0;						//소환수
+	float SkillCoolTime = 0.f;				//공격 스킬 쿨타임 감소
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+	float DashCoolTime = 0.f;				//이동 스킬 쿨타임 감소
+	
 };
